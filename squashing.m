cedb3d499d02ca5d4d562e3689a01852567ce3af
JohnAshburner <jashburner@gmail.com>
2015-04-13 19:15:00 +0100

    More tweaks.

--- squashing.m
+++ squashing.m
@@ -2,24 +2,24 @@ x = -5:0.01:5;
 
 subplot(3,2,1);
 plot(x,x>0,'k','LineWidth',2);
-xlabel('a^T x');
-ylabel('f(a^T x)');
+xlabel('f_*','Interpreter','tex');
+ylabel('\sigma(f_*)','Interpreter','tex');
 title('Heaviside step function');
 axis([[-5 5] -0.01 1.01]);
 %grid on
 
 subplot(3,2,3);
 plot(x,exp(x)./(exp(x)+1),'k','LineWidth',2);
-xlabel('a^T x');
-ylabel('f(a^T x)');
+xlabel('f_*');
+ylabel('\sigma(f_*)');
 title('Logistic function');
 axis([[-5 5] -0.01 1.01]);
 %grid on
 
 subplot(3,2,5);
 plot(x*sqrt(pi/8),normcdf(x*sqrt(pi/8)),'k','LineWidth',2);
-xlabel('a^T x');
-ylabel('f(a^T x)');
+xlabel('f_*');
+ylabel('\sigma(f_*)');
 title('Inverse Probit function (Normal CDF)');
 axis([[-5 5]*sqrt(pi/8) -0.01 1.01]);
 %grid on
